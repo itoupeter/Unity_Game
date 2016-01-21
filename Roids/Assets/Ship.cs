04116be671a75b0f2b95e4e1f2922d46bd5cc771
itoupeter <itoupeter@gmail.com>
2016-01-20 20:55:09 -0500

    update

---
+++ Roids/Assets/Ship.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+    public float turnSpeed;
+    public float thrustSpeed;
+
+	// Use this for initialization
+	void Start () {
+        turnSpeed = .5f;
+        thrustSpeed = .01f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( Input.GetAxisRaw( "Vertical" ) > 0 ){
+            gameObject.transform.Translate( 0, 0, thrustSpeed );
+        }
+
+        if( Input.GetAxisRaw( "Horizontal" ) < 0 ){
+            gameObject.transform.Rotate( 0, turnSpeed, 0 );
+        }else if( Input.GetAxisRaw( "Horizontal" ) > 0 ) {
+            gameObject.transform.Rotate( 0, -turnSpeed, 0 );
+        }
+    }
+}
